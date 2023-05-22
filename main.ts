344d0daed6551ec7e7b8f2a3ea1ad485117f5f90
Kobe Li <likobe23@gmail.com>
2023-05-22 12:50:06 -0700

    Add api calls

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { getUser } from './src/hackernews-api-v0';
+import { User } from './src/types/User';
 
 const app = express()
 const SERVER_PORT = 3000;
@@ -9,6 +11,13 @@ app.use(cors())
 app.get("/", (req: Request, res: Response) => {
     res.send({ message: "Hello, Express!" })
 });
+
+app.get("/user/:userId", async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    const userInfo = await getUser(userId);
+
+    res.send(userInfo)
+})
 
 app.listen(
     SERVER_PORT,
---
+++ src/hackernews-api-v0.ts
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { User } from "./types/User";
+import { Comment, Job, Poll, PollOption, Story } from "./types/Item";
+
+const HACKER_NEWS_API_ENDPOINT_BASE_URL = "https://hacker-news.firebaseio.com/v0/";
+
+export const getUser = async (username: string) => {
+    return axios.get<User>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/user/${username}.json`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch(err => {
+            throw new Error(`Encountered error when fetching info for user ${username}: ${err}`)
+        });
+}
+
+export const getStory = async (storyId: number) => {
+    return axios.get<Story>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/item/${storyId}.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching story ${storyId}: ${err}`);
+        });
+}
+
+export const getComment = async (commentId: number) => {
+    return axios.get<Comment>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/item/${commentId}.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching comment ${commentId}: ${err}`);
+        });
+}
+
+export const getJob = async (jobId: number) => {
+    return axios.get<Job>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/item/${jobId}.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching job ${jobId}: ${err}`);
+        });
+}
+
+export const getPoll = (pollId: number) => {
+    return axios.get<Poll>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/item/${pollId}.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching poll ${pollId}: ${err}`);
+        });
+}
+
+export const getPollOpt = (pollOptId: number) => {
+    return axios.get<PollOption>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/item/${pollOptId}.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching poll option ${pollOptId}: ${err}`);
+        });
+}
+
+export const getMaxItem = () => {
+    return axios.get<number>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/maxitem.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching max item: ${err}`);
+        });
+}
+
+export const getTopStories = () => {
+    return axios.get<number[]>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/topstories.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching top stories: ${err}`);
+        });
+}
+
+export const getAskStories = () => {
+    return axios.get<number[]>(`${HACKER_NEWS_API_ENDPOINT_BASE_URL}/askstories.json`)
+        .then(response => response.data)
+        .catch(err => {
+            throw new Error(`Encountered error when fetching ask stories: ${err}`);
+        });
+}
